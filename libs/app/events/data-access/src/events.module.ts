558211dbd58ab369d4208828a2fd15c8d76ee04c
Ayaaz Street <ayaazstreet@gmail.com>
2023-03-27 20:55:18 +0200

    🎉 feat/events (skeleton code)

---
+++ libs/app/events/data-access/src/events.module.ts
@@ -0,0 +1,12 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { AuthModule } from '@mp/app/auth/data-access';
+import { NgxsModule } from '@ngxs/store';
+import { EventsState } from './events.state';
+import { EventsApi } from './events.api';
+
+@NgModule({
+  imports: [CommonModule, AuthModule], NgxsModule.forFeature([EventsState])
+  providers: [EventsApi],
+})
+export class EventsModule {}
