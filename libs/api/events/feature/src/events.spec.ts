2cebf692cad0f98fb0548da0f0a90e466b16eba0
DvZ <danielvzyl1469@gmail.com>
2023-03-27 21:03:25 +0200

    🧪 (api) Added notifyAboutEvent test

--- libs/api/events/feature/src/events.spec.ts
+++ libs/api/events/feature/src/events.spec.ts
@@ -1,13 +1,15 @@
 import { EventsService } from './events.service';
 import { CommandBus } from '@nestjs/cqrs';
-import { IEvent, IEventResponse, GenerateEventEvent } from '@mp/api/events/util';
+import { IEvent, IEventResponse, GenerateEventEvent,
+    NotifyEventEvent } from '@mp/api/events/util';
 import { request } from 'http';
 
 describe('EventsService', () => {
     let eventService: EventsService;
     let cBus: CommandBus;
     let param: IEvent;
-    let request: GenerateEventEvent;
+    let request1: GenerateEventEvent;
+    let request2: NotifyEventEvent;
     let result: Promise<IEventResponse>;
 
     beforeEach(() => {
@@ -18,8 +20,17 @@ describe('EventsService', () => {
         it('should return a GenerateEventCommand',async () => {
             jest.spyOn(eventService, 'generateEvent').mockImplementation(() => result);
             param.eventId = "test";
-            request = new GenerateEventEvent(param);
-            expect(await eventService.generateEvent(request));
+            request1 = new GenerateEventEvent(param);
+            expect(await eventService.generateEvent(request1)).resolves.toEqual(result);
+        })
+    });
+
+    describe('notifyAboutEvent', () => {
+        it('should return a NotifyEventCommand',async () => {
+            jest.spyOn(eventService, 'notifyAboutEvent').mockImplementation(() => result);
+            param.eventId = "test";
+            request2 = new NotifyEventEvent(param);
+            expect(await eventService.notifyAboutEvent(request2)).resolves.toEqual(result);
         })
     });
 });
