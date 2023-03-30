8c3d8674ad9fc3fc17fc988e8f2793f677137c46
Azola <azola.m.lukhozi@gmail.com>
2023-03-30 14:56:28 +0200

    ✨ Skeleton Code For Post Files

--- libs/app/post/util/src/post.actions.ts
+++ libs/app/post/util/src/post.actions.ts
@@ -1,17 +1,9 @@
-import { IPost } from '@mp/api/post/util';
+//import { IPost } from '@mp/api/post/util';
 
 export class Logout {
   static readonly type = '[Post] Logout';
 }
 
-export class SubscribeToPost {
-  static readonly type = '[Post] SubscribeToPost';
-}
-
-export class SetPost {
-  static readonly type = '[Post] SetPost';
-  constructor(public readonly post: IPost | null) {}
-}
 
 export class UpdateAccountDetails {
   static readonly type = '[Post] UpdateAccountDetails';
