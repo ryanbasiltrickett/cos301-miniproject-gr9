6f85baffa137f4c1ae186bd77daccb72602b12e3
Jacob-Mahloko <114677637+Jacob-Mahloko@users.noreply.github.com>
2023-04-03 12:09:14 +0200

    ✅  updated the notification api functions

--- libs/app/notification/data-access/src/notification.api.ts
+++ libs/app/notification/data-access/src/notification.api.ts
@@ -1,47 +1,86 @@
 import { Injectable } from '@angular/core';
 import { doc, docData, Firestore } from '@angular/fire/firestore';
 import { Functions, httpsCallable } from '@angular/fire/functions';
-//create a notification object
-/*import{
-    sendNotificationRequest,
-    sendNotificationResponse
-    } from ''*/
+import{
+    ITags,
+    INotification,
+    INotifyEventRequest,
+    INotifyFriendActivityRequest,
+    INotifyLowTimeRequest,
+    INotifyRecievedTimeRequest,
+    INotifySavingFriendRequest,
+    INotifyEventResponse,
+    INotifyFriendActivityResponse,
+    INotifyLowTimeResponse,
+    INotifyRecievedTimeResponse,
+    INotifySavingFriendResponse
+    } from '@mp/api/2notification/util';
 
 @Injectable()
 //send functionality specified on the system requirement
 export class NotificationApi{
-    constructor(
-        private readonly firestore: Firestore,
-        private readonly functions: Functions
-      ) {}
+    
+  constructor(
+      private readonly firestore: Firestore,
+      private readonly functions: Functions
+    ) {}
 
-    async sendNotfication(id: string){
-        /*return await httpsCallable<
-        sendNotificationRequest,
-        sendNotificationResponse>(
-            this.functions,
-            'SendNotification'
-        )(request);*/
-    }
-
-    async fetchNotification(id: string){
-
-        /*const docRef = doc(
-            this.firestore,
-            `notifications/${id}`
-          ).withConverter<IProfile>({
-            fromFirestore: (snapshot) => {
-              return snapshot.data() as IProfile;
-            },
-            toFirestore: (it: IProfile) => it,
-          });
-          return docData(docRef, { idField: 'id' });
-        /*return await httpsCallable<
-        sendNotificationRequest,
-        sendNotificationResponse>(
-            this.functions,
-            'SendNotification'
-        )(request);*/
-    }
-
+  notification$(userId: ITags){
+    const docRef = doc(
+      this.firestore,
+      `notification/${userId}`
+    ).withConverter<INotification>({
+      fromFirestore: (snapshot) => {
+        return snapshot.data() as INotification;
+      },
+      toFirestore: (it: INotification) => it,
+    });
+    return docData(docRef, { idField: 'id' });
+  } 
+   
+  async notifyEvent(request:INotifyEventRequest){
+    return await httpsCallable<
+      INotifyEventRequest,
+      INotifyEventResponse
+    >(
+      this.functions,
+      'notifyEvent'
+    )(request);
+  }
+  async notifyFriendActivity(request : INotifyFriendActivityRequest){
+    return await httpsCallable<
+      INotifyFriendActivityRequest,
+      INotifyFriendActivityResponse
+    >(
+      this.functions,
+      'notifyFriendActivity'
+    )(request);
+  }
+  async notifyLowTime(request : INotifyLowTimeRequest){
+    return await httpsCallable<
+      INotifyLowTimeRequest,
+      INotifyLowTimeResponse
+      >(
+        this.functions,
+        'notifyLowTime'
+      )(request);
+  }
+  async notifySavingFriend(request : INotifySavingFriendRequest){
+    return await httpsCallable<
+      INotifySavingFriendRequest,
+      INotifySavingFriendResponse
+      >(
+        this.functions,
+        'notifySavingFriend'
+      )(request);
+  }
+  async notifyRecievedTime(request :  INotifyRecievedTimeRequest){
+    return await httpsCallable<
+    INotifyRecievedTimeRequest,
+    INotifyRecievedTimeResponse
+    >(
+      this.functions,
+      'notifyRecievedTime'
+    )(request);
+  }
 } 
