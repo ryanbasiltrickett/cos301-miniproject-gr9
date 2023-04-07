6a79380b8d79e9c77cf4d99a4953c8aab18d02d8
Skulderlock <78735770+SkulderLock@users.noreply.github.com>
2023-04-07 15:14:36 +0200

    🗃️ (db) Updated users repository

--- libs/api/users/util/src/interfaces/user.interface.ts
+++ libs/api/users/util/src/interfaces/user.interface.ts
@@ -6,6 +6,6 @@ export interface IUser {
   displayName?: string | null | undefined;
   photoURL?: string | null | undefined;
   phoneNumber?: string | null | undefined;
-  customClaims?: { [key: string]: any } | null | undefined;
+  // customClaims?: { [key: string]: any } | null | undefined;
   created?: Timestamp | null | undefined;
 }
