ca40b85ca849b42a89daa65fba8f320153999b19
Thuthuka Khumalo <thuthuka111@gmail.com>
2023-04-09 13:29:13 +0200

    🎨 (app) post upload form

--- libs/app/create/feature/src/create.module.ts
+++ libs/app/create/feature/src/create.module.ts
@@ -1,10 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { IonicModule } from '@ionic/angular';
 import { CreateRouting } from './create.routing';
 import { CreatePage } from './create.page.component';
 
 @NgModule({
-  imports: [CommonModule, CreateRouting],
+  imports: [CommonModule, IonicModule, CreateRouting],
   declarations: [CreatePage],
 })
 export class CreateModule {}
