9c5d2eccd115405a0732e66a8055d118fba5b59d
Thuthuka Khumalo <thuthuka111@gmail.com>
2023-04-09 17:37:04 +0200

    ♻️ (api) return NotImplemented for unimplemented Commands

--- libs/api/posts/feature/src/commands/comment-post.handler.ts
+++ libs/api/posts/feature/src/commands/comment-post.handler.ts
@@ -1,15 +1,13 @@
 import { CommandHandler, EventPublisher, ICommandHandler } from '@nestjs/cqrs';
 import { Timestamp } from 'firebase-admin/firestore';
 import { CommentPostCommand } from '@mp/api/posts/util';
-
+import { NotImplementedException } from '@nestjs/common';
 
 @CommandHandler(CommentPostCommand)
-export class CommentPostHandler
-  implements ICommandHandler<CommentPostCommand>
-{
+export class CommentPostHandler implements ICommandHandler<CommentPostCommand> {
   constructor(private publisher: EventPublisher) {}
 
   async execute(command: CommentPostCommand) {
-    console.log("Not implemented")
+    return NotImplementedException;
   }
 }
