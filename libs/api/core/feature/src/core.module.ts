1470616cbb340507de06ce0639f052736c16e2ae
Thuthuka Khumalo <thuthuka111@gmail.com>
2023-04-09 10:11:51 +0200

    ✨ (api) Posts Functions are included in api module

--- libs/api/core/feature/src/core.module.ts
+++ libs/api/core/feature/src/core.module.ts
@@ -1,10 +1,17 @@
 import { AuthModule } from '@mp/api/auth/feature';
 import { EventstoreModule } from '@mp/api/eventstore/feature';
+import { PostsModule } from '@mp/api/posts/feature';
 import { ProfilesModule } from '@mp/api/profiles/feature';
 import { UsersModule } from '@mp/api/users/feature';
 import { Module } from '@nestjs/common';
 
 @Module({
-  imports: [AuthModule, EventstoreModule, ProfilesModule, UsersModule],
+  imports: [
+    AuthModule,
+    EventstoreModule,
+    ProfilesModule,
+    UsersModule,
+    PostsModule,
+  ],
 })
 export class CoreModule {}
--- libs/api/posts/feature/src/posts.module.ts
+++ libs/api/posts/feature/src/posts.module.ts
@@ -1,1 +1,16 @@
-//TODO make this file like the profules.modules.ts file
+import { Module } from '@nestjs/common';
+import { CqrsModule } from '@nestjs/cqrs';
+import { PostsModule as PostsDataAccessModule } from '@mp/api/posts/data-access';
+import { PostService } from './posts.service';
+import { AddPostHandler, LikePostHandler } from './commands';
+import { PostLikedHandler } from './events';
+
+export const CommandHandlers = [AddPostHandler, LikePostHandler];
+export const EventHandlers = [PostLikedHandler];
+
+@Module({
+  imports: [CqrsModule, PostsDataAccessModule],
+  providers: [PostService, ...CommandHandlers, ...EventHandlers],
+  exports: [PostService],
+})
+export class PostsModule {}
