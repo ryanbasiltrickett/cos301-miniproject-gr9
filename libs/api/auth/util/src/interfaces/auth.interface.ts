2a6077b4f6524028480b2a8d53cd48cce6e7938c
Skulderlock <78735770+SkulderLock@users.noreply.github.com>
2023-04-10 12:26:09 +0200

    🗃️ (auth) Implemented auth.repository

--- libs/api/auth/util/src/interfaces/auth.interface.ts
+++ libs/api/auth/util/src/interfaces/auth.interface.ts
@@ -2,11 +2,11 @@ import { Timestamp } from 'firebase-admin/firestore';
 
 export interface IAuth {
   id: string;
-  email?: string | null | undefined;
+  email: string;
   displayName?: string | null | undefined;
   photoURL?: string | null | undefined;
   phoneNumber?: string | null | undefined;
-  customClaims?: { [key: string]: any } | null | undefined;
-  password?: string | null | undefined;
+  // customClaims?: { [key: string]: any } | null | undefined;
+  password: string;
   created?: Timestamp | null | undefined;
 }
