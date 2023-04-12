3a1c4bd965b432e20b534bb972e7b03d1c14c266
Ryan Trickett <ryanbasiltrickett@gmail.com>
2023-04-12 16:49:26 +0200

    🚧 (api) Newsfeed API Test Commented Out

--- libs/api/newsfeed/feature/src/newsfeed.spec.ts
+++ libs/api/newsfeed/feature/src/newsfeed.spec.ts
@@ -1,58 +1,58 @@
-import { CommandBus } from '@nestjs/cqrs';
-import {
-  INewsfeed, IgenNewsfeedResponse, IgenNewsfeedRequest,genNewsfeedEvent,
-  INFPost, IupdateNFPostResponse, IupdateNFPostRequest, updateNFPostEvent
-} from '@mp/api/newsfeed/util';
-import { NewsfeedService } from './newsfeed.service';
-import { request } from 'http';
-/*
-describe('NewsfeedService', () => {
+// import { CommandBus } from '@nestjs/cqrs';
+// import {
+//   INewsfeed, IgenNewsfeedResponse, IgenNewsfeedRequest,genNewsfeedEvent,
+//   INFPost, IupdateNFPostResponse, IupdateNFPostRequest, updateNFPostEvent
+// } from '@mp/api/newsfeed/util';
+// import { NewsfeedService } from './newsfeed.service';
+// import { request } from 'http';
+// /*
+// describe('NewsfeedService', () => {
 
-    let commandBus : CommandBus;
-    let INewsfeed : INewsfeed;
-    let newsfeedService: NewsfeedService;
-    let req: genNewsfeedEvent;
-    let reply: Promise<IgenNewsfeedResponse>;
+//     let commandBus : CommandBus;
+//     let INewsfeed : INewsfeed;
+//     let newsfeedService: NewsfeedService;
+//     let req: genNewsfeedEvent;
+//     let reply: Promise<IgenNewsfeedResponse>;
 
-    const t = "test";
+//     const t = "test";
 
-    beforeEach(() => {
-        newsfeedService = new NewsfeedService(commandBus);
-        INewsfeed.dud = t;
-    });
+//     beforeEach(() => {
+//         newsfeedService = new NewsfeedService(commandBus);
+//         INewsfeed.dud = t;
+//     });
 
-    describe('genNewsfeed', () => {
-        it('should work and return the genNewsfeed command',async () => {
-            jest.spyOn(newsfeedService, 'genNewsfeed').mockImplementation(() => reply);
-            INewsfeed.dud = t;//something wrong here
-            req = new genNewsfeedEvent(INewsfeed);
-            expect(await newsfeedService.genNewsfeed(req)).resolves.toEqual(reply);
-        })
-    });
-});
+//     describe('genNewsfeed', () => {
+//         it('should work and return the genNewsfeed command',async () => {
+//             jest.spyOn(newsfeedService, 'genNewsfeed').mockImplementation(() => reply);
+//             INewsfeed.dud = t;//something wrong here
+//             req = new genNewsfeedEvent(INewsfeed);
+//             expect(await newsfeedService.genNewsfeed(req)).resolves.toEqual(reply);
+//         })
+//     });
+// });
 
-describe('NewsfeedService', () => {
+// describe('NewsfeedService', () => {
 
-  let commandBus : CommandBus;
-  let INFPost : INFPost;
-  let newsfeedService: NewsfeedService;
-  let req: updateNFPostEvent;
-  let reply: Promise<IupdateNFPostResponse>;
+//   let commandBus : CommandBus;
+//   let INFPost : INFPost;
+//   let newsfeedService: NewsfeedService;
+//   let req: updateNFPostEvent;
+//   let reply: Promise<IupdateNFPostResponse>;
 
-  const t = "test";
+//   const t = "test";
 
-  beforeEach(() => {
-      newsfeedService = new NewsfeedService(commandBus);
-      INFPost.dud = t;
-  });
+//   beforeEach(() => {
+//       newsfeedService = new NewsfeedService(commandBus);
+//       INFPost.dud = t;
+//   });
 
-  describe('updateNFPost', () => {
-      it('should work and return the updateNFPost command',async () => {
-          jest.spyOn(newsfeedService, 'updateNFPost').mockImplementation(() => reply);
-          INFPost.dud = t;//something wrong here
-          req = new genNewsfeedEvent(INFPost);
-          expect(await newsfeedService.updateNFPost(req)).resolves.toEqual(reply);
-      })
-  });
-});
-*/
+//   describe('updateNFPost', () => {
+//       it('should work and return the updateNFPost command',async () => {
+//           jest.spyOn(newsfeedService, 'updateNFPost').mockImplementation(() => reply);
+//           INFPost.dud = t;//something wrong here
+//           req = new genNewsfeedEvent(INFPost);
+//           expect(await newsfeedService.updateNFPost(req)).resolves.toEqual(reply);
+//       })
+//   });
+// });
+// */
