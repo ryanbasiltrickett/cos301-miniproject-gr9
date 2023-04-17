1fce8cf36e433685b050b533c5228e41d87119dd
Jacob-Mahloko <114677637+Jacob-Mahloko@users.noreply.github.com>
2023-04-17 04:38:05 +0200

    🎨 added some actions in the state

--- libs/app/feed/data-access/src/feed.state.ts
+++ libs/app/feed/data-access/src/feed.state.ts
@@ -1,25 +1,43 @@
 import { Injectable } from '@angular/core';
-import {
-  IPost,
-  IgeneratePostRequest,
-  IupdateNFPostRequest
-} from '@mp/api/newsfeed/util'
+import { Action, Selector, State, StateContext, Store } from '@ngxs/store';
+import { SetError } from '@mp/app/errors/util';
 import {
   IProfile
 } from '@mp/api/profiles/util'
 import {
-  generatePost,setPage
+  IPost,
+  IgeneratePostRequest,
+  IupdateNFPostRequest,
+  IComment,
+  IFollowers,
+  INFProfile,
+  INFPost,
+  INewsfeed,
+  IPostArray,
+  IRecentPost,
+  IgenNewsfeedRequest
+
+} from '@mp/api/newsfeed/util'
+import {
+  generatePost,
+  setPage,
+  generateNewsFeed,
+  updateNewsFeedPost
 } from '@mp/app/feed/util'
-import { SetError } from '@mp/app/errors/util';
-import { Action, Selector, State, StateContext, Store } from '@ngxs/store';
+import { FeedApi } from './feed.api';
 import produce from 'immer';
 import { tap } from 'rxjs';
-import { FeedApi } from './feed.api';
+import { profile } from 'console';
 
 export interface FeedStateModel {
-  profile: IProfile | null;
+  profile: INFProfile | null;
+  limit:number | null | undefined;
+  feed: INewsfeed |null|undefined;
+  InfPost:INFPost |null;
   posts: IPost[] |null |undefined;
 }
+
+
 
 //Feel free to change this was just for testing purposes
 
@@ -27,7 +45,10 @@ export interface FeedStateModel {
   name: 'profile',
   defaults: {
     profile: null,
-    posts: null
+    limit:null,
+    feed:null,
+    posts: null,
+    InfPost:null,
   },
 })
 
@@ -53,23 +74,64 @@ export class FeedState {
   }
 
   @Action(generatePost)
-  async updateAccountDetails(ctx: StateContext<FeedStateModel>) {
+  async generatePost(ctx: StateContext<FeedStateModel>) {
     try {
       const state = ctx.getState();
       const userId = state.profile?.userId;
+      const timeLeft=state.profile?.timeLeft;
+      const limit=state.limit;
 
-      if (!userId)
+      if (!userId||!timeLeft||!limit)
         return ctx.dispatch(
           new SetError(
-            'UserId not set'
+            'UserId ,TimeLeft or limit not set'
           )
         );
 
-      const request: IgeneratePostRequest = {userID: userId}
-
+      const request: IgeneratePostRequest = {profile:{userId,timeLeft},limit};
       const responseRef = await this.FeedApi.generatePost(request);
       const response = responseRef.data;
       return ctx.dispatch(new setPage(response.posts.posts));
+    } catch (error) {
+      return ctx.dispatch(new SetError((error as Error).message));
+    }
+  }
+
+  @Action(generateNewsFeed)
+  async generateNewsFeed(ctx:StateContext<FeedStateModel>){
+    try {
+      const state=ctx.getState();
+      const feed=state.feed;
+
+      if(!feed)
+        return ctx.dispatch(
+          new SetError('Feed string not set')
+        );
+    
+      const request :IgenNewsfeedRequest={ dud:{}};
+      const responseRef =await this.FeedApi.generateNewsFeed(request);
+      const reponse =responseRef.data;
+      return ctx.dispatch(new SetError('what are we suuposed to do here')); 
+    } catch (error) {
+      return ctx.dispatch(new SetError((error as Error).message));
+    }
+  }
+
+  @Action(updateNewsFeedPost)
+  async updateNewsFeedPost(ctx: StateContext<FeedStateModel>){
+    try {
+      const state=ctx.getState();
+      const NfPost=state.InfPost;
+
+      if(!NfPost)
+        return ctx.dispatch(
+          new SetError('The NewsFeed Post not set')
+        );
+      
+      const request: IupdateNFPostRequest={dud:{}};
+      const responseRef=await this.FeedApi.updateNewsFeedPost(request);
+      const reponse=responseRef.data;
+      return ctx.dispatch(new SetError('What are we supposed to do here'));
     } catch (error) {
       return ctx.dispatch(new SetError((error as Error).message));
     }
