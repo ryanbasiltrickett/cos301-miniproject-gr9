623b727370e48abe506183554c1c76f4cc6e9ca4
Ryan Trickett <ryanbasiltrickett@gmail.com>
2023-04-24 14:34:05 +0200

    🐛 (app) Fixed Post Bug

--- libs/app/create/ui/src/create-post/create-post.component.ts
+++ libs/app/create/ui/src/create-post/create-post.component.ts
@@ -3,7 +3,6 @@ import { IProfile } from '@mp/api/profiles/util';
 import { ProfileState } from '@mp/app/profile/data-access';
 import { Select } from '@ngxs/store';
 import { Observable } from 'rxjs';
-import { FormBuilder, FormGroup } from '@angular/forms';
 import { CreatePost } from '@mp/app/create/util';
 import { Store } from '@ngxs/store';
 import { take } from 'rxjs/operators';
