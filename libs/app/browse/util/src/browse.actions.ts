1270f52153e200abcf724d8ec523f9d765ca61f6
DvZ <danielvzyl1469@gmail.com>
2023-04-30 13:27:30 +0200

    ⚰️ (app) Commented out dead code

--- libs/app/browse/util/src/browse.actions.ts
+++ libs/app/browse/util/src/browse.actions.ts
@@ -1,16 +1,16 @@
-export class BrowseAction{
-    static readonly type = '[Browse] Browse';
-    constructor(
-        public readonly search: {
-          userName: string
-        }
-      ) {}
-}
+// export class BrowseAction{
+//     static readonly type = '[Browse] Browse';
+//     constructor(
+//         public readonly search: {
+//           userName: string
+//         }
+//       ) {}
+// }
 
-export class ClearBrowseAction{
-  static readonly type = '[Browse] Clear';
-}
+// export class ClearBrowseAction{
+//   static readonly type = '[Browse] Clear';
+// }
 
-export class GetTrendingAction{
-  static readonly type = '[Browse] Trending';
-}
+// export class GetTrendingAction{
+//   static readonly type = '[Browse] Trending';
+// }
